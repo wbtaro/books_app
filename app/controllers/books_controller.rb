12dca5c31214bc67a8ab35cfdd71beba6346dc42
wbtaro <58219039+wbtaro@users.noreply.github.com>
2020-04-02 21:53:36 +0900

    Update app/controllers/books_controller.rb
    devide long line to two lines
    
    Co-Authored-By: Masaki Komagata <komagata@gmail.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
 
     if @book.save
-      redirect_to @book, notice: I18n.t("results.common.create", resource: I18n.t("activerecord.models.book.one"))
+      redirect_to @book, notice: t("results.common.create", resource: I18n.t("activerecord.models.book.one"))
     else
       render :new
     end
