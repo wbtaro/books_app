41e10c7a5af3bb19b7201a392f939e0b7f1b6dcc
Matsushima Ryotaro <m.ryotaro.325@gmail.com>
2020-04-09 10:08:57 +0900

    Fix: revise ApplicationController#set_commenters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,16 +45,19 @@ class ApplicationController < ActionController::Base
     end
 
     def set_commenters(commentable_resource)
-      temp_commenters = User.includes(:comments).
-                          where(comments: { commentable_id: commentable_resource.id, commentable_type: commentable_resource.class.to_s })
       @commenters = {}
       @posted_comments.each do |comment|
-        temp_commenters.each { |commenter| @commenters[comment.id] = commenter.name if comment.user_id == commenter.id }
+        commenter_list(commentable_resource).each { |commenter| @commenters[comment.id] = commenter.name if comment.user_id == commenter.id }
       end
     end
 
     def set_new_comment(commentable_resource)
       @comment = Comment.new(commentable_id: commentable_resource.id, commentable_type: commentable_resource.class, user_id: current_user.id)
+    end
+
+    def commenter_list(commentable_resource)
+      User.includes(:comments).
+            where(comments: { commentable_id: commentable_resource.id, commentable_type: commentable_resource.class.to_s })
     end
 
   protected
